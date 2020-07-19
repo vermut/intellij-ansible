32543f21a9bc38c908374a8a8327a3579f09a231
Pavels Veretennikovs <PVeretennikovs@riskfocus.com>
2020-07-19 16:09:58 +0300

    fix: tests (although we don't rely on them much)

--- src/test/java/lv/kid/vermut/intellij/yaml/parser/ParserTest.java
+++ src/test/java/lv/kid/vermut/intellij/yaml/parser/ParserTest.java
@@ -8,7 +8,7 @@ public class ParserTest extends ParsingTestCase {
 
     public ParserTest() {
         super("", "yml", new NeonParserDefinition());
-        PlatformTestCase.initPlatformLangPrefix();
+        PlatformTestCase.doAutodetectPlatformPrefix();
     }
 
     @Override
@@ -16,7 +16,7 @@ public class ParserTest extends ParsingTestCase {
         return "src/test/data/parser";
     }
 
-    protected void doTest(boolean checkResult, boolean suppressErrors) {
+    protected void myDoTest(boolean checkResult, boolean suppressErrors) {
         doTest(true);
         if (!suppressErrors) {
             Assert.assertFalse(
@@ -27,94 +27,94 @@ public class ParserTest extends ParsingTestCase {
     }
 
     public void test01() {
-        doTest(true, false);
+        myDoTest(true, false);
     }
 
     public void test02() {
-        doTest(true, false);
+        myDoTest(true, false);
     }
 
     public void test03() {
-        doTest(true, false);
+        myDoTest(true, false);
     }
 
     public void test04() {
-        doTest(true, false);
+        myDoTest(true, false);
     }
 
     public void test05() {
-        doTest(true, false);
+        myDoTest(true, false);
     }
 
     public void test06() {
-        doTest(true, false);
+        myDoTest(true, false);
     }
 
     public void test07() {
-        doTest(true, false);
+        myDoTest(true, false);
     }
 
     public void test08() {
-        doTest(true, false);
+        myDoTest(true, false);
     }
 
     public void test09() {
-        doTest(true, false);
+        myDoTest(true, false);
     }
 
     public void test10() {
-        doTest(true, false);
+        myDoTest(true, false);
     }
 
     public void test11() {
-        doTest(true, true);
+        myDoTest(true, true);
     }
 
     public void test12() {
-        doTest(true, true);
+        myDoTest(true, true);
     }
 
     public void test13() {
-        doTest(true, false);
+        myDoTest(true, false);
     }
 
     public void test14() {
-        doTest(true, true);
+        myDoTest(true, true);
     }
 
     public void test15() {
-        doTest(true, true);
+        myDoTest(true, true);
     }
 
     public void test16() {
-        doTest(true, true);
+        myDoTest(true, true);
     }
 
     public void test17() {
-        doTest(true, false);
+        myDoTest(true, false);
     }
 
     public void test18() {
-        doTest(true, true);
+        myDoTest(true, true);
     }
 
     public void testIssue31() {
-        doTest(true, false);
+        myDoTest(true, false);
     }
 
     public void testIssue31_simple() {
-        doTest(true, false);
+        myDoTest(true, false);
     }
 
     public void testIssue40() {
-        doTest(true, false);
+        myDoTest(true, false);
     }
 
     public void testIssue42() {
-        doTest(true, false);
+        myDoTest(true, false);
     }
 
     public void testIssue41() {
-        doTest(true, false);
+        myDoTest(true, false);
     }
 }
