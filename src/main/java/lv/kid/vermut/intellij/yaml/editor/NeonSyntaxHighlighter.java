63d1206538412dd9fe7abd95447ef2172378f9d6
Pavels Veretennikovs <PVeretennikovs@riskfocus.com>
2020-10-18 20:33:58 +0300

    fix: make TextAttributes uniqie
    fixes #93, fixes #49, fixes #101, fixes #100, fixes #102, fixes #93

--- src/main/java/lv/kid/vermut/intellij/yaml/editor/NeonSyntaxHighlighter.java
+++ src/main/java/lv/kid/vermut/intellij/yaml/editor/NeonSyntaxHighlighter.java
@@ -20,74 +20,51 @@ import static lv.kid.vermut.intellij.yaml.parser.NeonElementTypes.JINJA;
 
 public class NeonSyntaxHighlighter extends SyntaxHighlighterBase {
 
-	public static final String            UNKNOWN_ID       = "Bad character";
-	public static final TextAttributesKey UNKNOWN          = TextAttributesKey.createTextAttributesKey(UNKNOWN_ID, HighlighterColors.BAD_CHARACTER);
+    public static final TextAttributesKey UNKNOWN = TextAttributesKey.createTextAttributesKey("ANSIBLE.BAD_CHARACTER", HighlighterColors.BAD_CHARACTER);
+    public static final TextAttributesKey COMMENT = TextAttributesKey.createTextAttributesKey("ANSIBLE.COMMENT", DefaultLanguageHighlighterColors.LINE_COMMENT);
+    public static final TextAttributesKey IDENTIFIER = TextAttributesKey.createTextAttributesKey("ANSIBLE.IDENTIFIER", DefaultLanguageHighlighterColors.INSTANCE_FIELD);
+    public static final TextAttributesKey INTERPUNCTION = TextAttributesKey.createTextAttributesKey("ANSIBLE.INTERPUNCTION", DefaultLanguageHighlighterColors.DOT);
+    public static final TextAttributesKey NUMBER = TextAttributesKey.createTextAttributesKey("ANSIBLE.NUMBER", DefaultLanguageHighlighterColors.NUMBER);
+    public static final TextAttributesKey KEYWORD = TextAttributesKey.createTextAttributesKey("ANSIBLE.KEYWORD", DefaultLanguageHighlighterColors.KEYWORD);
+    public static final TextAttributesKey STRING = TextAttributesKey.createTextAttributesKey("ANSIBLE.STRING", DefaultLanguageHighlighterColors.STRING);
+    public static final TextAttributesKey TAG = TextAttributesKey.createTextAttributesKey("ANSIBLE.TAG", DefaultLanguageHighlighterColors.MARKUP_ATTRIBUTE);
+    public static final TextAttributesKey JINJA_CODE = TextAttributesKey.createTextAttributesKey("ANSIBLE.JINJA", DefaultLanguageHighlighterColors.TEMPLATE_LANGUAGE_COLOR);
 
-	public static final String            COMMENT_ID       = "Comment";
-	public static final TextAttributesKey COMMENT          = TextAttributesKey.createTextAttributesKey(COMMENT_ID, DefaultLanguageHighlighterColors.LINE_COMMENT);
-
-	public static final String            IDENTIFIER_ID    = "Identifier";
-	public static final TextAttributesKey IDENTIFIER       = TextAttributesKey.createTextAttributesKey(IDENTIFIER_ID, DefaultLanguageHighlighterColors.INSTANCE_FIELD);
-
-	public static final String            INTERPUNCTION_ID = "Interpunction";
-	public static final TextAttributesKey INTERPUNCTION    = TextAttributesKey.createTextAttributesKey(INTERPUNCTION_ID, DefaultLanguageHighlighterColors.DOT);
-
-	public static final String            NUMBER_ID        = "Number";
-	public static final TextAttributesKey NUMBER           = TextAttributesKey.createTextAttributesKey(NUMBER_ID, DefaultLanguageHighlighterColors.NUMBER);
-
-	public static final String            KEYWORD_ID       = "Keyword";
-	public static final TextAttributesKey KEYWORD          = TextAttributesKey.createTextAttributesKey(KEYWORD_ID, DefaultLanguageHighlighterColors.KEYWORD);
-
-	public static final String            STRING_ID       = "STRING";
-	public static final TextAttributesKey STRING          = TextAttributesKey.createTextAttributesKey(STRING_ID, DefaultLanguageHighlighterColors.STRING);
-
-	public static final String            TAG_ID          = "TAG";
-	public static final TextAttributesKey TAG             = TextAttributesKey.createTextAttributesKey(TAG_ID, DefaultLanguageHighlighterColors.MARKUP_ATTRIBUTE);
-
-	public static final String            REFERENCE_ID    = "REFERENCE";
-	public static final TextAttributesKey REFERENCE       = TextAttributesKey.createTextAttributesKey(REFERENCE_ID, DefaultLanguageHighlighterColors.TEMPLATE_LANGUAGE_COLOR);
-
-	public static final String            JINJA_ID        = "JINJA";
-	public static final TextAttributesKey JINJA_CODE           = TextAttributesKey.createTextAttributesKey(JINJA_ID, DefaultLanguageHighlighterColors.TEMPLATE_LANGUAGE_COLOR);
-
-	// Groups of IElementType's
-	public static final TokenSet sBAD           = TokenSet.create(NEON_UNKNOWN);
-	public static final TokenSet sCOMMENTS      = TokenSet.create(NEON_COMMENT);
-	public static final TokenSet sIDENTIFIERS   = TokenSet.create(NEON_KEY);
-	public static final TokenSet sINTERPUNCTION = TokenSet.create(NEON_LBRACE_CURLY, NEON_RBRACE_CURLY, NEON_LBRACE_SQUARE, NEON_RBRACE_SQUARE, NEON_ITEM_DELIMITER, NEON_ASSIGNMENT);
-	public static final TokenSet sKEYWORDS      = TokenSet.create(NEON_KEYWORD);
-	public static final TokenSet sSTRINGS       = TokenSet.create(NEON_STRING);
-	public static final TokenSet sTAGS          = TokenSet.create(NEON_TAG, NEON_HEADER);
-
-	public static final TokenSet sJINJA         = TokenSet.create(JINJA, NEON_LBRACE_JINJA, NEON_RBRACE_JINJA, NEON_IDENTIFIER);
+    // Groups of IElementType's
+    public static final TokenSet sBAD = TokenSet.create(NEON_UNKNOWN);
+    public static final TokenSet sCOMMENTS = TokenSet.create(NEON_COMMENT);
+    public static final TokenSet sIDENTIFIERS = TokenSet.create(NEON_KEY);
+    public static final TokenSet sINTERPUNCTION = TokenSet.create(NEON_LBRACE_CURLY, NEON_RBRACE_CURLY, NEON_LBRACE_SQUARE, NEON_RBRACE_SQUARE, NEON_ITEM_DELIMITER, NEON_ASSIGNMENT);
+    public static final TokenSet sKEYWORDS = TokenSet.create(NEON_KEYWORD);
+    public static final TokenSet sSTRINGS = TokenSet.create(NEON_STRING);
+    public static final TokenSet sTAGS = TokenSet.create(NEON_TAG, NEON_HEADER);
+    public static final TokenSet sJINJA = TokenSet.create(JINJA, NEON_LBRACE_JINJA, NEON_RBRACE_JINJA, NEON_IDENTIFIER);
 
 
-	// Static container
-	private static final Map<IElementType, TextAttributesKey> ATTRIBUTES = new HashMap<IElementType, TextAttributesKey>();
+    // Static container
+    private static final Map<IElementType, TextAttributesKey> ATTRIBUTES = new HashMap<IElementType, TextAttributesKey>();
 
+    // Fill in the map
+    static {
+        fillMap(ATTRIBUTES, sBAD, UNKNOWN);
+        fillMap(ATTRIBUTES, sCOMMENTS, COMMENT);
+        fillMap(ATTRIBUTES, sIDENTIFIERS, IDENTIFIER);
+        fillMap(ATTRIBUTES, sINTERPUNCTION, INTERPUNCTION);
+        fillMap(ATTRIBUTES, sKEYWORDS, KEYWORD);
+        fillMap(ATTRIBUTES, sSTRINGS, STRING);
+        fillMap(ATTRIBUTES, sTAGS, TAG);
+        fillMap(ATTRIBUTES, sJINJA, JINJA_CODE);
+    }
 
-	// Fill in the map
-	static {
-		fillMap(ATTRIBUTES, sBAD,           UNKNOWN);
-		fillMap(ATTRIBUTES, sCOMMENTS,      COMMENT);
-		fillMap(ATTRIBUTES, sIDENTIFIERS,   IDENTIFIER);
-		fillMap(ATTRIBUTES, sINTERPUNCTION, INTERPUNCTION);
-		fillMap(ATTRIBUTES, sKEYWORDS,      KEYWORD);
-		fillMap(ATTRIBUTES, sSTRINGS,       STRING);
-		fillMap(ATTRIBUTES, sTAGS,  	    TAG);
-		fillMap(ATTRIBUTES, sJINJA,  	    JINJA_CODE);
-	}
+    @NotNull
+    @Override
+    public Lexer getHighlightingLexer() {
+        return new NeonHighlightingLexer(new NeonLexer());
+    }
 
-
-	@NotNull
-	@Override
-	public Lexer getHighlightingLexer() {
-		return new NeonHighlightingLexer(new NeonLexer());
-	}
-
-	@NotNull
-	@Override
-	public TextAttributesKey[] getTokenHighlights(IElementType type) {
-		return pack(ATTRIBUTES.get(type));
-	}
+    @NotNull
+    @Override
+    public TextAttributesKey[] getTokenHighlights(IElementType type) {
+        return pack(ATTRIBUTES.get(type));
+    }
 }
