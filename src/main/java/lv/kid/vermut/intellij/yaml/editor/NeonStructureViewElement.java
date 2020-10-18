e6f6779be4394ea4772cd0b5c0313dedbbc68e7e
Pavels Veretennikovs <PVeretennikovs@riskfocus.com>
2020-10-18 20:51:15 +0300

    fix: hide NPE in getLocationString
    fixes #81

--- src/main/java/lv/kid/vermut/intellij/yaml/editor/NeonStructureViewElement.java
+++ src/main/java/lv/kid/vermut/intellij/yaml/editor/NeonStructureViewElement.java
@@ -19,79 +19,83 @@ import java.util.List;
  */
 public class NeonStructureViewElement extends PsiTreeElementBase<PsiElement> {
 
-	public NeonStructureViewElement(PsiElement element) {
-		super(element);
-	}
+    public NeonStructureViewElement(PsiElement element) {
+        super(element);
+    }
 
-	@NotNull
-	@Override
-	public Collection<StructureViewTreeElement> getChildrenBase() {
-		List<StructureViewTreeElement> elements = new ArrayList<StructureViewTreeElement>();
-		PsiElement element = getElement();
+    @NotNull
+    @Override
+    public Collection<StructureViewTreeElement> getChildrenBase() {
+        List<StructureViewTreeElement> elements = new ArrayList<StructureViewTreeElement>();
+        PsiElement element = getElement();
 
-		if (element instanceof NeonFile) {
-			if (element.getChildren().length == 1 && element.getChildren()[0] instanceof NeonArray) { // top level array -> show it's elements
-				addArrayElements(elements, element.getChildren()[0]);
+        if (element instanceof NeonFile) {
+            if (element.getChildren().length == 1 && element.getChildren()[0] instanceof NeonArray) { // top level array -> show it's elements
+                addArrayElements(elements, element.getChildren()[0]);
 
-			} else { // file children directly
-				addArrayElements(elements, element);
-			}
+            } else { // file children directly
+                addArrayElements(elements, element);
+            }
 
-		} else if (element instanceof NeonKeyValPair && ((NeonKeyValPair) element).getValue() instanceof NeonArray) {
-			addArrayElements(elements, ((NeonKeyValPair) element).getValue());
+        } else if (element instanceof NeonKeyValPair && ((NeonKeyValPair) element).getValue() instanceof NeonArray) {
+            addArrayElements(elements, ((NeonKeyValPair) element).getValue());
 
-		} else if (element instanceof NeonArray) {
-			addArrayElements(elements, element);
+        } else if (element instanceof NeonArray) {
+            addArrayElements(elements, element);
 
-		}
+        }
 
-		return elements;
-	}
+        return elements;
+    }
 
-	private void addArrayElements(List<StructureViewTreeElement> elements, PsiElement firstValue) {
-		for (PsiElement child : firstValue.getChildren()) {
-			elements.add(new NeonStructureViewElement(child));
-		}
-	}
+    private void addArrayElements(List<StructureViewTreeElement> elements, PsiElement firstValue) {
+        for (PsiElement child : firstValue.getChildren()) {
+            elements.add(new NeonStructureViewElement(child));
+        }
+    }
 
-	@Override
-	@Nullable
-	public String getPresentableText() {
-		PsiElement element = getElement();
-		if (element instanceof NeonFile) {
-			return ((NeonFile) element).getName();
+    @Override
+    @Nullable
+    public String getPresentableText() {
+        PsiElement element = getElement();
+        if (element instanceof NeonFile) {
+            return ((NeonFile) element).getName();
 
-		} else if (element instanceof NeonArray) {
-			return "array";
+        } else if (element instanceof NeonArray) {
+            return "array";
 
-		} else if (element instanceof NeonKeyValPair) {
-			return ((NeonKeyValPair) element).getKeyText();
+        } else if (element instanceof NeonKeyValPair) {
+            return ((NeonKeyValPair) element).getKeyText();
 
-		}  else if (element instanceof NeonKey) {
-			return ((NeonKey) element).getKeyText();
+        } else if (element instanceof NeonKey) {
+            return ((NeonKey) element).getKeyText();
 
-		}  else if (element instanceof NeonValue) {
-			return element.getText();
+        } else if (element instanceof NeonValue) {
+            return element.getText();
 
-		} else {
-			return "? YAML";
-		}
-	}
+        } else {
+            return "? YAML";
+        }
+    }
 
-	@Override
-	@Nullable
-	public String getLocationString() {
-		PsiFile containingFile = getElement().getContainingFile();
+    @Override
+    @Nullable
+    public String getLocationString() {
+        try {
+            PsiFile containingFile = getElement().getContainingFile();
 
-		return "("
-				+ containingFile.getParent().getParent().getName() + "/"
-				+ containingFile.getParent().getName() + "/"
-				+ containingFile.getName() + ")";
-	}
+            return "("
+                    + containingFile.getParent().getParent().getName() + "/"
+                    + containingFile.getParent().getName() + "/"
+                    + containingFile.getName() + ")";
+        } catch (NullPointerException ignored) {
+            return null;
+        }
+    }
 
-	@Nullable
-	@Override
-	public Icon getIcon(boolean unused) {
-		return YamlIcons.FILETYPE_ICON;
-	}
+    @Nullable
+    @Override
+    public Icon getIcon(boolean unused) {
+        return YamlIcons.FILETYPE_ICON;
+    }
 }
